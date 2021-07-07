030bf20d87f52e5120297ad093a398fee4fb9602
hamada456 <73261938+hamada456@users.noreply.github.com>
2021-07-07 16:45:59 +0900

    07071645

--- class.js
+++ class.js
@@ -1,24 +1,44 @@
 (() => {
-    const $elm = document.querySelector("#js-accordion");
-    const $trigger = $elm.getElementsByTagName("a");
 
-    const triggerLen = $trigger.length;
-    let index = 0;
-    while (index < triggerLen) {
-        $trigger[index].addEventListener("click", (e) => clickHandler(e))
-        index++;
+class Accordion {
+
+    constructor(obj){
+
+        console.log("obj", obj.hookName);
+
+        const $elm = document.querySelector(obj.hookName);
+        const $trigger = $elm.getElementsByTagName(obj.tagName);
+    
+        const triggerLen = $trigger.length;
+        let index = 0;
+        while (index < triggerLen) {
+            $trigger[index].addEventListener("click", (e) => this.clickHandler(e))
+            index++;
+        }
     }
 
-    const clickHandler = (e) => {
-        e.preventDefault();
+        clickHandler(e){
+            e.preventDefault();
 
-        const $target = e.currentTarget;
-        const $content = $target.nextElementSibling;
+            const $target = e.currentTarget;
+            const $content = $target.nextElementSibling;
 
-        if($content.style.display === "block"){
-            $content.style.display = "none";
-        } else {
-            $content.style.display = "block";
+            if($content.style.display === "block"){
+                $content.style.display = "none";
+            } else {
+                $content.style.display = "block";
         }
-    };
+    }
+}
+
+const fuckingAccordion = new Accordion({
+    hookName: "#js-faq",
+    tagName: "p"
+});
+
+const dummyAccordion = new Accordion({
+    hookName: "#js-accordion",
+    tagName: "a"
+});
+
 })();
