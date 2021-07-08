a6b356ca9f4fcac841b53e9bcb9e7a1294731805
hamada456 <73261938+hamada456@users.noreply.github.com>
2021-07-08 13:52:45 +0900

    修正

--- class2.js
+++ class2.js
@@ -2,6 +2,11 @@
     const $elm = document.querySelector("js-accordion");
     const $trigger = $elm.getElementsByTagName("a");
 
-    $trigger[0].addEventListener("click",(e) => clickHandler(e));
+    $trigger[0].addEventListener("click", (e) => clickHandler(e));
+
+    const clickHandler = (e) => {
+        e.preventDefault();
+        console.log("!!!!");
+    };
 
 })();
