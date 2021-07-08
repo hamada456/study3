a22efe81551b4c069364fda7409f1aafb5f904b5
hamada456 <73261938+hamada456@users.noreply.github.com>
2021-07-08 13:38:13 +0900

    0にクリックイベンされたらclickHandlerを実行

--- class2.js
+++ class2.js
@@ -1,4 +1,7 @@
 (() => {
     const $elm = document.querySelector("js-accordion");
     const $trigger = $elm.getElementsByTagName("a");
+
+    $trigger[0].addEventListener("click",(e) => clickHandler(e));
+
 })();
