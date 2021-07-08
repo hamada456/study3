4a6c6067516392cda0e66dc0e6a135fd72716735
hamada456 <73261938+hamada456@users.noreply.github.com>
2021-07-08 15:17:04 +0900

    リファクタリング１

--- class2.js
+++ class2.js
@@ -2,6 +2,13 @@
     const $elm = document.querySelector("#js-accordion");
     const $trigger = $elm.getElementsByTagName("a");
 
+    const triggerLen = $trigger.length;
+    let index = 0;
+    while(index < triggerLen){
+        $trigger[index].addEventListener("click",(e) => clickHandler(e));
+        index++;
+    }
+
     $trigger[0].addEventListener("click", (e) => clickHandler(e));
 
     const clickHandler = (e) => {
